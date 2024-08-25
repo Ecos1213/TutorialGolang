b994dbb3c3a473c48c3f3d348cb49bfe888a4961
Ecos1213 <diasflac1213@gmail.com>
2024-08-25 13:22:50 -0500

    Paquetes de terceros: se explicó cómo agregar paquetes de terceros a un proyecto de Go utilizando el comando go get, y cómo administrar las dependencias del proyecto utilizando los archivos go.mod y go.sum.

--- hola.go
+++ hola.go
@@ -1,15 +1,24 @@
-// con el comando en la consola go env nos permite saber donde esta el path principal de golang donde se tendra que colocar el proyecto para correrlo sin problemas, se puede dejar en otra carpeta pero no es recomendado ya que en el path principal tiene carpetas que usa golang en esto esta la carpeta bin que es donde dejara nuestra compilacion y el pkg que son paquetes de proyectos terceros que usaremos en nuestro proyecto
-// la extension de archivo de go es .go asi tenemos que crear archivos de golang
-// con go version podemos saber que version de golang tenemos
-// podemos trabajar en playground de golang pero ten en cuenta que tiene limitaciones por ejemplo no podemos acceder al sistema operativo, tampoco podemos usar paquetes de terceros y no podemos crear un software dinamico esto quiere decir que no podemos crear aplicaciones donde el usuario necesite colocar datos como por ejemplo tomar datos de la consola
-// con el simbolo // se crea comentarios en golang
-package main // esto hace referencia a que este archivo o paquete se llamara main todos los paquetes main tienen una funcion principal llamada main que esta declarada mas adelante y al ejecutarse automaticamente ejecutara primero esa funcion
+// los paquetes terceros son paquetes que no estan incluidos en las libreria estandard de go como el paquete fmt, estos paquetes terceros han sido desarrollado y mantenidos por las comunidades de programadores de go o por otros desarrolladores de go
+// para trabajar con paquetes de terceros (paquetes externos) tenemos que inicializar un manejador de modulos para nuestra aplicacion, entonces para inicializar un manejador de modulos para nuestra aplicacion vamos a ejcutar el siguiente comando dentro de nuestro proyecto, el comando es go mod, para el manejador de modulos y con init vamos a  inicializar el manejador de modulos despues de init sigue el nombre con el cual vamos a manejar el modulo de nuestro proyecto
+// go mod init nombredemanejadordemodulos
+// este comando creara un archivo llamado go.mod, el cual contiene el nombre del manejador del modulo (module holamundo) y una version de go (go 1.23.0)
+// el archivo go.mod es un archivo que se utiliza para definir y gestionar los modulos y las dependencias de nuestro proyecto en go, tambien cuando empezemos a descargar y utilizar los archivos externos tambien se va a crear un archivo llamado go.sum, este archivo go.sum es un archivo se utiliza para registrar la suma de verificacion de los modulos y las dependencias de nuestro proyecto
+// ahora vamos a utilizar el paquete cod, es un paquete externo de tercero para el lenguaje de programacion go que proporciona una serie de citas famosas como el hola mundo
+// con el comando go get rutaderepositoriodelpaquete descargamos el paquete y agregarlo a nuestro proyecto, este puede estar en github como tambien puede estar en otra parte el paquete quite se descarga de la siguiente manera
+// go get rsc.io/quote
+// al hacer esto descargara el paquete y simeplemente agregara el modulo en el archivo go.mod y crear el archivo go.sum con el respectivo modulo
 
-import "fmt" // con import podemos importar paquetes que necesite golang, el paquete fmt es un paquete principal de golang (no es un paquete tercero) y este paquete principalmente lo que hace es tomar e imprimir datos osea input y output de datos
+package main
 
-func main() { // como todos los archivos main tiene una funcion llamada main, que lo que hace es ejecutar esta funcion ya que es la principal, para crear una funcion usamos la palabra reservada func
-	fmt.Println("Hola Mundo") // cuando importamos el pauquete podemos usar el objeto fmt para llamar funciones en este caso usamos Println, esta funcion tiene como parametro un string que lo que hace es imprimir en consola el texto que pasamos en
+import (
+	"fmt"
+
+	"rsc.io/quote"
+) // en el import para agregar varios paquetes tenemos que usar el parentesis y dentro del parentesis agregamos todos los paquetes que vamos a usar, para agregar el paquete externo simplemente colocamos la ruta del paquete que es rsc.io/quote
+
+func main() {
+	fmt.Println("Hola Mundo")
+	fmt.Println(quote.Hello()) // ahora podemos usar el objeto quote y llamar la funcion anidada Hello que tiene quote esto nos imprimira Hello world
 }
 
-// si queremos compilar internamente y correr el archivo, en la terminal podemos dar el comando go run nombredelarchivo.go, lo que hace esto es compilar (lo compila en la carpeta principal y en bin donde se instalo golang coloca la compilacion) y a la vez ejecutara el archivo mostrandonos hola mundo, pero no nos dejara la compilacion
-// si queremos compilar y dejar el archivo para produccion podemos usar el comando go build nombredelarchivo.go, esto compilara el archivo y nos lo dejara en la carpeta del archivo compilado, si queremos ejecutar simplemente escribimos en consola hola.exe (esto solo sucede en windows) en linux seria ./hola
+//con go run hola.go ejecutamos nuestro programa en consola
